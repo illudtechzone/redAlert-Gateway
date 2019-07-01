6970a0ab49a511d56d4635050630937638ebbbfa
sooraj_pn <you@example.com>
2019-07-01 15:02:57 +0530

    swaggerd with friends microservice

---
+++ src/main/java/com/illud/redalert/client/friends_microservice/model/User.java
@@ -0,0 +1,144 @@
+package com.illud.redalert.client.friends_microservice.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.illud.redalert.client.friends_microservice.model.User;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * User
+ */
+@Validated
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-07-01T14:46:22.957+05:30[Asia/Kolkata]")
+
+public class User   {
+  @JsonProperty("friends")
+  @Valid
+  private List<User> friends = null;
+
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("userId")
+  private String userId = null;
+
+  public User friends(List<User> friends) {
+    this.friends = friends;
+    return this;
+  }
+
+  public User addFriendsItem(User friendsItem) {
+    if (this.friends == null) {
+      this.friends = new ArrayList<User>();
+    }
+    this.friends.add(friendsItem);
+    return this;
+  }
+
+  /**
+   * Get friends
+   * @return friends
+  **/
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public List<User> getFriends() {
+    return friends;
+  }
+
+  public void setFriends(List<User> friends) {
+    this.friends = friends;
+  }
+
+  public User name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public User userId(String userId) {
+    this.userId = userId;
+    return this;
+  }
+
+  /**
+   * Get userId
+   * @return userId
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return Objects.equals(this.friends, user.friends) &&
+        Objects.equals(this.name, user.name) &&
+        Objects.equals(this.userId, user.userId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(friends, name, userId);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class User {\n");
+    
+    sb.append("    friends: ").append(toIndentedString(friends)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    userId: ").append(toIndentedString(userId)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
